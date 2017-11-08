11b95054ee6ff3e5a8bf59329e5e7d5bc1649496
Martyn Janes <martyn@obany.com>
2017-11-08 08:02:33 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# UniteJS Test Matrix - React Webpack
+
+Repo to run a multitude of configuration options for UniteJS on React Application Framework.
+
+[![Build Status][travis-image]][travis-url]
+
+[travis-url]: https://travis-ci.org/unitejs-test-matrix/re-webpack-matrix/
+[travis-image]: http://img.shields.io/travis/unitejs-test-matrix/re-webpack-matrix/master.svg?style=flat
---
+++ create-apps.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+#
+# React
+#
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-js-jas-pro --title="React Webpack JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Jest --unitTestFramework=Jasmine --unitTestEngine=JSDom --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=PostCss --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-js-jas-wdr --title="React Webpack JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=PostCss --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-js-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-js-mch-pro --title="React Webpack JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=PhantomJS --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=PostCss --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-js-mch-wdr --title="React Webpack JavaScript" --sourceLanguage=JavaScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=ESLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=PostCss --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-js-mch-wdr
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-ts-jas-pro --title="React Webpack TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=Protractor --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Css --cssPost=None --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-pro
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-ts-jas-wdr --title="React Webpack TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=Jasmine --unitTestEngine=PhantomJS --e2eTestRunner=WebdriverIO --e2eTestFramework=Jasmine --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Sass --cssPost=None --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-jas-wdr
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-ts-mch-pro --title="React Webpack TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=Protractor --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Less --cssPost=None --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-pro
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-pro
+
+node unitejs/cli/bin/unite configure --packageName=re-wpk-ts-mch-wdr --title="React Webpack TypeScript" --sourceLanguage=TypeScript --moduleType=CommonJS --bundler=Webpack --unitTestRunner=Karma --unitTestFramework=MochaChai --unitTestEngine=ChromeHeadless --e2eTestRunner=WebdriverIO --e2eTestFramework=MochaChai --linter=TSLint --license=MIT --packageManager=Npm --cssPre=Stylus --cssPost=None --appFramework=React --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=class --name=MyClass --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=component --name=MyComponent --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=enum --name=MyEnum --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-wdr
+node unitejs/cli/bin/unite generate --type=interface --name=MyInterface --disableVersionCheck --outputDirectory=./apps/re-wpk-ts-mch-wdr
